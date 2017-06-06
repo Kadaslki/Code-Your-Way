f5d72fdfc5fab035a159e187fc1a76e483fa00a2
Vivek Kumar Gautam <Kadaslki@users.noreply.github.com>
2017-06-06 20:52:28 +0530

    Create TheCoinChangeProblem.cpp

---
+++ Dynamic Programming/TheCoinChangeProblem.cpp
@@ -0,0 +1,25 @@
+Problem Description: https://www.hackerrank.com/challenges/coin-change
+
+Problem Solution:
+
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int m,n,x,y,i,j;
+    cin>>n>>m;
+    
+    int c[m];
+    long long int t[n+1]={0};
+    for(i=0;i<m;i++)
+        cin>>c[i];
+    t[0]=1;
+    
+    for(int i=0; i<m; i++)
+        for(int j=c[i]; j<=n; j++)
+            t[j] += t[j-c[i]];
+    
+    cout<<t[n];
+}
